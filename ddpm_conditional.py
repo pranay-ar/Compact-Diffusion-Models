bfd1c40ad97b25891174ead0b4796700b0122b52
pranay-ar <a.pranay2001@gmail.com>
2023-12-05 02:06:03 +0000

    fix model ckpt load

--- ddpm_conditional.py
+++ ddpm_conditional.py
@@ -91,8 +91,11 @@ def train(configs):
     model = torch.nn.DataParallel(model)
     if use_distillation:
         teacher = UNet_conditional(num_classes=num_classes).to(device)
-        teacher.load_state_dict(args.teacher_path if args.teacher_path is not None else \
-            "/work/pi_adrozdov_umass_edu/pranayr_umass_edu/cs682/Diffusion-Models-pytorch/models/DDPM_conditional/ema_ckpt.pt")
+        ckpt = torch.load(
+            args.teacher_path if args.teacher_path is not None else \
+            "/work/pi_adrozdov_umass_edu/pranayr_umass_edu/cs682/Diffusion-Models-pytorch/models/DDPM_conditional/ema_ckpt.pt"
+            )
+        teacher.load_state_dict(ckpt)
         teacher = torch.nn.DataParallel(teacher)
         teacher.eval()
         for param in teacher.parameters():
